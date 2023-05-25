ef3b51d0bdf7f6c887890aa909b807d959755417
zoniventris <f.zoniventris@gmail.com>
2023-05-25 00:54:45 -0700

    Remove destroyEvent from JobTogglesPresenter
    This functionality is now covered by the LifetimeEvents callbacks on the
    infobox

--- Assets/Scripts/UI/JobTogglesPresenter.cs
+++ Assets/Scripts/UI/JobTogglesPresenter.cs
@@ -7,9 +7,6 @@ public class JobTogglesPresenter : MonoBehaviour
     [SerializeField]
     private GameObject togglePrefab;
 
-    [SerializeField]
-    private GameObjectGameEvent destroyEvent;
-
     private class ToggleJobPair
     {
         public TogglePresenter Toggle { get; private set; }
@@ -108,23 +105,5 @@ public class JobTogglesPresenter : MonoBehaviour
         {
             toggles[i].SetJob(null);
         }
-    }
-
-    private void OnObjectDestroyed(object sender, GameObject destroyedObject)
-    {
-        if (destroyedObject == JobSource)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void OnEnable()
-    {
-        destroyEvent.OnGameEvent += OnObjectDestroyed;
-    }
-
-    private void OnDisable()
-    {
-        destroyEvent.OnGameEvent -= OnObjectDestroyed;
     }
 }
