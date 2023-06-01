9422ded3a8e1d06456c824486cf523f4451c86af
zoniventris <f.zoniventris@gmail.com>
2023-05-31 22:50:27 -0700

    Add ConditionalCommand

---
+++ Assets/Scripts/Actors/Commands/ConditionalCommand.cs
@@ -0,0 +1,28 @@
+public class ConditionalCommand : ICommand
+{
+    public IProvider<bool> ShouldExecute { get; private set; }
+    public ICommand Command { get; private set; }
+
+    private VariableProvider<bool> didExecute = new VariableProvider<bool>();
+    public IProvider<bool> DidExecute => didExecute;
+
+    public ConditionalCommand(IProvider<bool> shouldExecute, ICommand command)
+    {
+        ShouldExecute = shouldExecute;
+        Command = command;
+    }
+
+    public ICommand.State Execute()
+    {
+        if (!didExecute.Value)
+        {
+            return ICommand.State.Stopped;
+        }
+        return Command.Execute();
+    }
+
+    public void Init()
+    {
+        didExecute.Value = ShouldExecute.Get();
+    }
+}
