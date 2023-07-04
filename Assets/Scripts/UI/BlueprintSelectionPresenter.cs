a70148f6ca5f30c54cb65ed5d0ca7f7e5405605b
zoniventris <f.zoniventris@gmail.com>
2023-07-04 16:24:27 -0700

    Display blueprints in selected build category

---
+++ Assets/Scripts/UI/BlueprintSelectionPresenter.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BlueprintSelectionPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private BuildCatalog buildCatalog;
+
+    [SerializeField]
+    private GameObject buttonPrefab;
+
+    private BuildCategory currentCategory;
+    public BuildCategory CurrentCategory
+    {
+        get => currentCategory;
+        set
+        {
+            currentCategory = value;
+            gameObject.SetActive(currentCategory != null);
+        }
+    }
+
+    private List<BlueprintPresenter> children = new List<BlueprintPresenter>();
+
+    private void UpdateButtons()
+    {
+        if (CurrentCategory == null)
+        {
+            foreach (var child in children)
+            {
+                child.Blueprint = null;
+            }
+        }
+        else
+        {
+            IEnumerable<Blueprint> blueprints = buildCatalog.GetBlueprintsInCategory(CurrentCategory).Where(b => buildCatalog.IsUnlocked(b));
+            int numDisplayed = blueprints.Count();
+            for (int i = 0; i < numDisplayed; i++)
+            {
+                BlueprintPresenter child;
+                if (i >= children.Count)
+                {
+                    child = Instantiate(buttonPrefab, transform).GetComponent<BlueprintPresenter>();
+                    children.Add(child);
+                }
+                else
+                {
+                    child = children[i];
+                }
+                child.Blueprint = blueprints.ElementAt(i);
+            }
+
+            for (int i = numDisplayed; i < children.Count; i++)
+            {
+                children[i].Blueprint = null;
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        UpdateButtons();
+    }
+}
--- Assets/Scripts/UI/BuildCategoryPresenter.cs
+++ Assets/Scripts/UI/BuildCategoryPresenter.cs
@@ -1,16 +1,74 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BuildCategoryPresenter : MonoBehaviour
 {
     [SerializeField]
     private BuildCatalog buildCatalog;
+    public BuildCatalog BuildCatalog => buildCatalog;
 
     [SerializeField]
     private GameObject buttonPrefab;
 
-    private List<TogglePresenter> categoryToggles = new List<TogglePresenter>();
+    [SerializeField]
+    private UnityEvent<BuildCategory> onSelectCategory;
+    public UnityEvent<BuildCategory> OnSelectCategory => onSelectCategory;
+
+    private List<ToggleCategoryPair> categoryToggles = new List<ToggleCategoryPair>();
+
+    private class ToggleCategoryPair
+    {
+        public TogglePresenter Toggle { get; private set; }
+        public BuildCategoryPresenter Owner { get; private set; }
+
+        private BuildCategory category;
+        public BuildCategory Category
+        {
+            get => category;
+            set
+            {
+                category = value;
+                UpdateDisplayedInfo();
+            }
+        }
+
+        public ToggleCategoryPair(TogglePresenter toggle, BuildCategoryPresenter owner)
+        {
+            Toggle = toggle;
+            Toggle.OnClick.AddListener(HandleClick);
+            Owner = owner;
+        }
+
+        private void HandleClick(bool currentValue)
+        {
+            if (currentValue)
+            {
+                Toggle.IsOn = false;
+                Owner.OnSelectCategory.Invoke(null);
+            }
+            else
+            {
+                Toggle.IsOn = true;
+                Owner.OnSelectCategory.Invoke(Category);
+            }
+        }
+
+        private void UpdateDisplayedInfo()
+        {
+            if (category == null)
+            {
+                Toggle.gameObject.SetActive(false);
+            }
+            else
+            {
+                Toggle.SetLabel(category.DisplayName);
+                Toggle.Interactable = Owner.BuildCatalog.IsUnlocked(category);
+                Toggle.gameObject.SetActive(true);
+            }
+        }
+    }
 
     private void UpdateDisplayedCategories()
     {
@@ -18,25 +76,23 @@ public class BuildCategoryPresenter : MonoBehaviour
         int numDisplayed = categories.Count();
         for (int i = 0; i < numDisplayed; i++)
         {
-            TogglePresenter toggle;
+            ToggleCategoryPair toggle;
             if (i >= categoryToggles.Count)
             {
-                toggle = Instantiate(buttonPrefab, transform).GetComponent<TogglePresenter>();
+                var t = Instantiate(buttonPrefab, transform).GetComponent<TogglePresenter>();
+                toggle = new ToggleCategoryPair(t, this);
                 categoryToggles.Add(toggle);
             }
             else
             {
                 toggle = categoryToggles[i];
-                toggle.gameObject.SetActive(true);
             }
-            var category = categories.ElementAt(i);
-            toggle.SetLabel(category.DisplayName);
-            toggle.Interactable = buildCatalog.IsUnlocked(category);
+            toggle.Category = categories.ElementAt(i);
         }
 
         for (int i = numDisplayed; i < categoryToggles.Count; i++)
         {
-            categoryToggles[i].gameObject.SetActive(false);
+            categoryToggles[i].Category = null;
         }
     }
 
