b7e25383396c46afb4995b08cf32e184239864fa
zoniventris <f.zoniventris@gmail.com>
2023-07-10 15:35:58 -0700

    Generate terrain at runtime

---
+++ Assets/Scripts/TerrainBuilder.cs
@@ -0,0 +1,38 @@
+using Unity.AI.Navigation;
+using UnityEngine;
+
+public class TerrainBuilder : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject defaultObject;
+
+    [SerializeField]
+    private Vector3Int mapSize;
+
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField]
+    private NavMeshSurface navMeshSurface;
+
+    private void Awake()
+    {
+        BuildTerrain();
+    }
+
+    private void BuildTerrain()
+    {
+        for (int y = 0; y < mapSize.y; y++)
+        {
+            for (int z = 0; z < mapSize.z; z++)
+            {
+                for (int x = 0; x < mapSize.x; x++)
+                {
+                    GameObject go = Instantiate(defaultObject, new Vector3(x * grid.cellSize.x, y * grid.cellSize.y, z * grid.cellSize.z), Quaternion.identity);
+                    go.transform.parent = transform;
+                }
+            }
+        }
+        navMeshSurface.BuildNavMesh();
+    }
+}
