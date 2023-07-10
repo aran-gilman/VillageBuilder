c357a5d7ef38651694778d995ed3a3da1a7011db
zoniventris <f.zoniventris@gmail.com>
2023-07-10 13:49:31 -0700

    Change appearance of model when previewing

--- Assets/Scripts/BuildMode/DisplayPreview.cs
+++ Assets/Scripts/BuildMode/DisplayPreview.cs
@@ -5,6 +5,9 @@ public class DisplayPreview : MonoBehaviour
     [SerializeField]
     private BuildModeState buildModeState;
 
+    [SerializeField]
+    private Material previewMaterial;
+
     private GameObject previewObject;
 
     private void OnEnable()
@@ -15,6 +18,10 @@ public class DisplayPreview : MonoBehaviour
             Destroy(previewObject);
         }
         previewObject = Instantiate(buildModeState.SelectedBlueprint.PreviewModel, transform);
+        foreach (MeshRenderer renderer in previewObject.GetComponentsInChildren<MeshRenderer>())
+        {
+            renderer.materials = CreateMaterialArray(renderer.materials.Length);
+        }
     }
 
     private void OnDisable()
@@ -23,5 +30,15 @@ public class DisplayPreview : MonoBehaviour
         {
             Destroy(previewObject);
         }
+    }
+
+    private Material[] CreateMaterialArray(int size)
+    {
+        Material[] materials = new Material[size];
+        for (int i = 0; i < size; i++)
+        {
+            materials[i] = previewMaterial;
+        }
+        return materials;
     }
 }
